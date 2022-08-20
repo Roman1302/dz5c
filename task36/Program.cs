d17c3e042315cc12458da476634540195475a115
Роман Бухмистов <rrb13@mail.ru>
2022-08-21 00:43:51 +0300

    Доработал 36 задачу

--- task36/Program.cs
+++ task36/Program.cs
@@ -3,3 +3,34 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0 */
 
+int a = 8;
+int[] ArrayOutput()
+{
+    Console.Write("[");
+    int[] array = new int[a];
+    var random = new Random();
+
+    for (int i = 0; i < a; i++)
+    {
+        array[i] = random.Next(0, 99);
+        Console.Write(array[i]);
+        if (i < a - 1)
+            Console.Write(", ");
+    }
+    Console.Write("]");
+    return array;
+}
+
+var array = ArrayOutput();
+
+Console.WriteLine();
+
+int result = 0;
+int lengts = array.Length;
+
+for (int j = 1; j < lengts; j += 2)
+{
+    int temp = int.Parse(array[j] + "");
+    result = temp + result;
+}
+Console.WriteLine(result);
